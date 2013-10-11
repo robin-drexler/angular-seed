8fa677ceb10fb869c5da54a4cf7e57911dab76de
robin <robin@jimdo.com>
2013-10-11 12:02:05 +0200

    some directive

--- app/js/directives.js
+++ app/js/directives.js
@@ -8,4 +8,16 @@ angular.module('myApp.directives', []).
     return function(scope, elm, attrs) {
       elm.text(version);
     };
-  }]);
+  }])
+    .directive('vote', function() {
+        var definition = {
+            restrict: 'E',
+            replace: true,
+            template: '<div style="width: 50px; height:  50px; border:  1px solid black;"></div>',
+            link: function(scope, elm) {
+                elm.text(33);
+            }
+        }
+
+        return definition;
+    });
